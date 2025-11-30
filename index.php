f3b635ba6328961a58f17778e9a825d1bdfca453
Ayesha Naseer <ayeshanaseer19999@gmail.com>
2025-11-30 21:29:18 +0500

    add index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'sections/header.php'; ?>
+<?php include 'sections/hero.php'; ?>
+<?php include 'sections/features.php'; ?>
+<?php include 'sections/pricing.php'; ?>
+<?php include 'sections/contact.php'; ?>
+<?php include 'sections/footer.php'; ?>
