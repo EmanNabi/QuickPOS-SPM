5a013f12076415e8733977ce60152b420ead6292
EmanNabi <f223626@cfd.nu.edu.pk>
2025-11-30 20:29:14 +0500

    [QLP-7] Added header and navigation

---
+++ sections/header.php
@@ -0,0 +1,9 @@
+<header>
+    <img src="assets/images/logo.png" class="logo" alt="QuickPOS Logo">
+    <nav>
+        <a href="#features">Features</a>
+        <a href="#pricing">Pricing</a>
+        <a href="#contact">Contact</a>
+    </nav>
+    <button class="signup">Sign Up</button>
+</header>
